9a35ba15eec230817c839f01e49d93637525e180
Alex Bajan <alex.i.bajan@gmail.com>
2021-09-17 05:44:36 +0300

    just checking

--- 16. Asynchronous JS  Async-Await  Promises  AJAX/script.js
+++ 16. Asynchronous JS  Async-Await  Promises  AJAX/script.js
@@ -481,7 +481,7 @@ PART 1
 1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
 If this part is too tricky for you, just watch the first part of the solution.
 PART 2
-2. Comsume the promise using .then and also add an error handler;
+2. Consume the promise using .then and also add an error handler;
 3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
 4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
 5. After the second image has loaded, pause execution for 2 seconds again;
@@ -489,3 +489,46 @@ PART 2
 TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
 GOOD LUCK 😀
 */
+
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// const imageContainer = document.querySelector('.images');
+// let currentImage;
+
+// const createImage = function (imgPath) {
+//   return new Promise(function (resolve, reject) {
+//     const image = document.createElement('img');
+//     image.src = imgPath;
+
+//     image.addEventListener('load', () => {
+//       imageContainer.append(image);
+//       resolve(image);
+//     });
+
+//     image.addEventListener('error', () => {
+//       reject(new Error('Could not load image.'));
+//     });
+//   });
+// };
+
+// createImage('./img/img-1.jpg')
+//   .then(image => {
+//     currentImage = image;
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImage.style.display = 'none';
+//     return createImage('./img/img-2.jpg');
+//   })
+//   .then(image => {
+//     currentImage = image;
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImage.style.display = 'none';
+//   })
+//   .catch(err => console.error(err));
