d5fa1315326e1570e17a20cdecc3847669f34992
Alex Bajan <alex.i.bajan@gmail.com>
2021-09-18 23:40:38 +0300

    17 and counting

---
+++ 17. Modern JavaScript development - Modules&Tooling/176 - Lets fix some bad code - part 1.js
@@ -0,0 +1,116 @@
+var budget = [
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+];
+
+var limits = {
+  jonas: 1500,
+  matilda: 100,
+};
+
+var add = function (value, description, user) {
+  if (!user) user = 'jonas';
+  user = user.toLowerCase();
+
+  var lim;
+  if (limits[user]) {
+    lim = limits[user];
+  } else {
+    lim = 0;
+  }
+
+  if (value <= lim) {
+    budget.push({ value: -value, description: description, user: user });
+  }
+};
+add(10, 'Pizza 🍕');
+add(100, 'Going to movies 🍿', 'Matilda');
+add(200, 'Stuff', 'Jay');
+console.log(budget);
+
+var check = function () {
+  for (var el of budget) {
+    var lim;
+    if (limits[el.user]) {
+      lim = limits[el.user];
+    } else {
+      lim = 0;
+    }
+
+    if (el.value < -lim) {
+      el.flag = 'limit';
+    }
+  }
+};
+check();
+
+console.log(budget);
+
+var bigExpenses = function (limit) {
+  var output = '';
+  for (var el of budget) {
+    if (el.value <= -limit) {
+      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
+    }
+  }
+  output = output.slice(0, -2); // Remove last '/ '
+  console.log(output);
+};
+///////////------------------
+
+// Part 1:
+const budget = [
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+];
+
+const spendingLimits = {
+  jonas: 1500,
+  matilda: 100,
+};
+
+const getLimit = user => spendingLimits?.[user] ?? 0;
+// if the spending limit does not exist then  it will return undefined and from there the nullish coalescence returns its right-hand side operand when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand, in our case 0.
+
+const addExpense = function (value, description, user = 'jonas') {
+  user = user.toLowerCase();
+
+  if (value <= getLimit(user)) {
+    budget.push({ value: -value, description, user });
+  }
+};
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
+
+const checkExpenses = function () {
+  for (const entry of budget)
+    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
+};
+checkExpenses();
+
+const logBigExpenses = function (bigLimit) {
+  let output = '';
+  for (const entry of budget)
+    output +=
+      entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
+
+  output = output.slice(0, -2); // Remove last '/ '
+  console.log(output);
+};
+
+console.log(budget);
+
+logBigExpenses(500);
--- 17. Modern JavaScript development - Modules&Tooling/177 - Lets fix some bad code - part 2.js
+++ 17. Modern JavaScript development - Modules&Tooling/177 - Lets fix some bad code - part 2.js
@@ -1,4 +1,70 @@
-'use strict';
+var budget = [
+  { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
+  { value: -45, description: 'Groceries 🥑', user: 'jonas' },
+  { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
+  { value: 300, description: 'Freelancing 👩‍💻', user: 'jonas' },
+  { value: -1100, description: 'New iPhone 📱', user: 'jonas' },
+  { value: -20, description: 'Candy 🍭', user: 'matilda' },
+  { value: -125, description: 'Toys 🚂', user: 'matilda' },
+  { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
+];
+
+var limits = {
+  jonas: 1500,
+  matilda: 100,
+};
+
+var add = function (value, description, user) {
+  if (!user) user = 'jonas';
+  user = user.toLowerCase();
+
+  var lim;
+  if (limits[user]) {
+    lim = limits[user];
+  } else {
+    lim = 0;
+  }
+
+  if (value <= lim) {
+    budget.push({ value: -value, description: description, user: user });
+  }
+};
+add(10, 'Pizza 🍕');
+add(100, 'Going to movies 🍿', 'Matilda');
+add(200, 'Stuff', 'Jay');
+console.log(budget);
+
+var check = function () {
+  for (var el of budget) {
+    var lim;
+    if (limits[el.user]) {
+      lim = limits[el.user];
+    } else {
+      lim = 0;
+    }
+
+    if (el.value < -lim) {
+      el.flag = 'limit';
+    }
+  }
+};
+check();
+
+console.log(budget);
+
+var bigExpenses = function (limit) {
+  var output = '';
+  for (var el of budget) {
+    if (el.value <= -limit) {
+      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
+    }
+  }
+  output = output.slice(0, -2); // Remove last '/ '
+  console.log(output);
+};
+//////////////
+// Part 2
+('use strict');
 
 const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
@@ -11,16 +77,12 @@ const budget = Object.freeze([
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
 ]);
 
-// Object has been freezed and cannot be changed by adding a new property but the properties inside the entries can be changed...
-budget[0].value = 10000;
-
 const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
 });
-// const limit = spendingLimits[user] ? spendingLimits[user] : 0; // alternative way of getting the limit
+
 const getLimit = (limits, user) => limits?.[user] ?? 0;
-// if the spending limit does not exist then  it will return undefined and from there the nullish coalescence returns its right-hand side operand when its left-hand side operand is null or undefined, and otherwise returns its left-hand side operand, in our case 0.
 
 // Pure function:
 const addExpense = function (
@@ -45,17 +107,6 @@ const newBudget2 = addExpense(
   'Matilda'
 );
 const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
-//
-
-// const checkExpenses2 = function (state, limits) {
-//   return state.map(entry => {
-//     return entry.value < -getLimit(limits, entry.user)
-//       ? { ...entry, flag: 'limit' }
-//       : entry;
-//   });
-//   // for (const entry of newBudget3)
-//   //   if (entry.value < -getLimit(limits, entry.user)) entry.flag = 'limit';
-// };
 
 // Pure function:
 const checkExpenses = (state, limits) =>
@@ -68,21 +119,14 @@ const checkExpenses = (state, limits) =>
 const finalBudget = checkExpenses(newBudget3, spendingLimits);
 console.log(finalBudget);
 
-// IMpure function:
+// Impure function:
 const logBigExpenses = function (state, bigLimit) {
   const bigExpenses = state
     .filter(entry => entry.value <= -bigLimit)
     .map(entry => entry.description.slice(-2))
     .join(' / ');
-  // .reduce((str, cur) => `${str} / ${cur.description.slice(-2)}`, '');
 
   console.log(bigExpenses);
-  // let output = '';
-  // for (const entry of budget)
-  //   output +=
-  //     entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
-  // output = output.slice(0, -2); // Remove last '/ '
-  // console.log(output);
 };
 
 logBigExpenses(finalBudget, 500);
