0c671ef320389398073800d32afa1c2a52ff1603
Alex Bajan <alex.i.bajan@gmail.com>
2021-09-21 10:56:49 +0300

    problem solved!

--- 18. Forkify app/src/js/controller.js
+++ 18. Forkify app/src/js/controller.js
@@ -1,3 +1,10 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+//
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -8,6 +15,33 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
+/////////////////////////////////////////
+// https://forkify-api.herokuapp.com/v2//
+/////////////////////////////////////////
 
-///////////////////////////////////////
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    // Guard clause:
+    if (!id) return;
+
+    // Load spinner:
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe:
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe:
+    recipeView.render(model.state.recipe);
+    //
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
+// window.addEventListener('hashchange', controlRecipes);
+// window.addEventListener('load', controlRecipes);
